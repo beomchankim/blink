c2ae027df88f50ce67803ac42d40020bd69fd920
beomchankim <78011531+beomchankim@users.noreply.github.com>
2022-05-19 00:29:29 +0900

    test-01

---
+++ terraform/modules/resourcegroup/variables.tf
@@ -0,0 +1,12 @@
+variable "resouce_group_name" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "resouce_group_name" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
